7c9fae0ca3d5f502db09202eb53b1c596874b8a9
hvguzman <guzman.hannahvania@gmail.com>
2017-09-11 16:38:17 +0800

    draft of capstone 1

---
+++ js/app.js
@@ -0,0 +1,80 @@
+$(document).foundation();
+
+/*
+  Switch actions
+*/
+$('.unmask').on('click', function(){
+
+  if($(this).prev('input').attr('type') == 'password')
+    changeType($(this).prev('input'), 'text');
+
+  else
+    changeType($(this).prev('input'), 'password');
+
+  return false;
+});
+
+function changeType(x, type) {
+  if(x.prop('type') == type)
+  return x; //That was easy.
+  try {
+    return x.prop('type', type); //Stupid IE security will not allow this
+  } catch(e) {
+    //Try re-creating the element (yep... this sucks)
+    //jQuery has no html() method for the element, so we have to put into a div first
+    var html = $("<div>").append(x.clone()).html();
+    var regex = /type=(\")?([^\"\s]+)(\")?/; //matches type=text or type="text"
+    //If no match, we add the type attribute to the end; otherwise, we replace
+    var tmp = $(html.match(regex) == null ?
+      html.replace(">", ' type="' + type + '">') :
+      html.replace(regex, 'type="' + type + '"') );
+    //Copy data from old element
+    tmp.data('type', x.data('type') );
+    var events = x.data('events');
+    var cb = function(events) {
+      return function() {
+            //Bind all prior events
+            for(i in events)
+            {
+              var y = events[i];
+              for(j in y)
+                tmp.bind(i, y[j].handler);
+            }
+          }
+        }(events);
+        x.replaceWith(tmp);
+    setTimeout(cb, 10); //Wait a bit to call function
+    return tmp;
+  }
+}
+
+
+// START CLOCK SCRIPT
+
+Number.prototype.pad = function(n) {
+  for (var r = this.toString(); r.length < n; r = 0 + r);
+  return r;
+};
+
+function updateClock() {
+  var now = new Date();
+  var milli = now.getMilliseconds(),
+    sec = now.getSeconds(),
+    min = now.getMinutes(),
+    hou = now.getHours(),
+    mo = now.getMonth(),
+    dy = now.getDate(),
+    yr = now.getFullYear();
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
+    corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
+  for (var i = 0; i < tags.length; i++)
+    document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+}
+
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
+
+// END CLOCK SCRIPT
